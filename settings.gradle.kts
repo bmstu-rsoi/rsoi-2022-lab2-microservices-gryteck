b462caf92c035f4e9949225a1c2b094ebbaa0cab
gryteck <112859317+gryteck@users.noreply.github.com>
2022-11-14 22:38:41 +0300

    gradlew

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "rsoi-2022-lab2-microservices-gryteck"
+
+// Common
+include("common")
+
+// Api
+include("bonus-service-api")
+include("ticket-service-api")
+include("flight-service-api")
+include("user-service-api")
+
+// Service
+include("config-server")
+include("gateway")
+include("ticket-service")
+include("user-service")
+include("bonus-service")
+include("flight-service")
+
